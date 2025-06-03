c33146beeb1d11b5a47dfb8462b3899975f113bc
Prakhar983 <154242556+Prakhar983@users.noreply.github.com>
2025-06-03 11:08:19 +0530

    Updated Version for Testing

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'my-pwa-v1.0.9'; // Update this for new versions
+const CACHE_NAME = 'my-pwa-v1.1.0'; // Update this for new versions
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
