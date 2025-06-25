6e80b27c0c9b27e265f75f5d73dfb6603a1c448c
Prakhar983 <154242556+Prakhar983@users.noreply.github.com>
2025-06-25 09:58:58 +0530

    updated the app

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@ self.addEventListener('install', (event) => {
       return cache.addAll([
         '/blue-qrdrop/', // Root path (adjust if your app is in a subdirectory)
         '/blue-qrdrop/index.html', // Path to index.html
-        '/blue-qrdrop/static/js/main.b7bba89c.js', // Path to JS bundle
+        '/blue-qrdrop/static/js/main.7218fd4b.js', // Path to JS bundle
         '/blue-qrdrop/static/css/main.975f4e65.css', // Path to CSS file
         // Add other assets here
       ]);
